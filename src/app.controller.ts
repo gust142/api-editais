968ec2f756d45fc34607b2f701d9e8ddb007628d
Gustavo Farias <gustavogfarias142@gmail.com>
2025-03-28 11:33:57 -0300

    teste rota workflow

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { IsPublic } from './auth/decorators/is-public.decorator';
 
-@Controller()
+@Controller("teste")
+@IsPublic()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return "ola teste worlflow"
   }
 }
